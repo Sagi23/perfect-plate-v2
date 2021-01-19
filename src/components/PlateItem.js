8082c7231f9fb23d8b4827d19cb8f3c18e6f4358
Sagi Twig <twigsagi@gmail.com>
2021-01-19 04:39:22 +0200

    Add delete icon

--- src/components/PlateItem.js
+++ src/components/PlateItem.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Icon } from "@iconify/react";
+import outlineDeleteForever from "@iconify-icons/ic/outline-delete-forever";
 
 function PlateItem({ item, amount, pro, cal, fat, id, handleClick }) {
   return (
@@ -8,9 +10,12 @@ function PlateItem({ item, amount, pro, cal, fat, id, handleClick }) {
       <p className="pro1">{pro},</p>
       <p className="cal1">{cal},</p>
       <p className="fat1">{fat}</p>
-      <button className="btn btn-1" onClick={() => handleClick(id)}>
-        X
-      </button>
+      <Icon
+        style={{ cursor: "pointer" }}
+        className="btn-1"
+        icon={outlineDeleteForever}
+        onClick={() => handleClick(id)}
+      />
     </div>
   );
 }
